98640e2f144655d5925c2adb2ecfa3063f726f17
0rose7 <sangokas36@gmail.com>
2024-06-26 22:03:50 +0300

    Genre Repository, book creation,roles,login and register

---
+++ src/main/java/com/spring/BookReservations/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.spring.BookReservations.controller;
+
+import com.spring.BookReservations.model.Book;
+import com.spring.BookReservations.service.BookService;
+import com.spring.BookReservations.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    @Autowired
+    BookService bookService;
+
+    @Autowired
+    GenreService genreService;
+
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        List<String> genres = genreService.findAllGenreNames();
+        System.out.println("Genres: " + genres);
+        model.addAttribute("genres", genres);
+        model.addAttribute("book", new Book());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createSubmit(@ModelAttribute Book book, Model model) {
+        bookService.save(book);
+        return "dashboard";
+    }
+}
