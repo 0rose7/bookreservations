f60fcddc418864142f8d9a81b9836bfb26a90f2a
0rose7 <sangokas36@gmail.com>
2024-06-26 20:53:51 +0300

    first commit

---
+++ src/test/java/com/example/bookreservations/BookReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookreservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
